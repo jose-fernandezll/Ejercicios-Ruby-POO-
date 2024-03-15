bf4341188cd016dc711d862c370b88ef81089400
jose-fernandezll <jose.ismael.fernandez001@gmail.com>
2024-03-14 20:58:02 -0500

    defined person class

---
+++ Person.rb
@@ -0,0 +1,14 @@
+class Person
+  attr_accessor :name, :age
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
+end
+
+p1 = Person.new('paco', 32)
+p2 = Person.new('mario',64)
+
+puts "su nombre es: #{p1.name} y su edad es #{p1.age}"
+puts "su nombre es: #{p2.name} y su edad es #{p2.age}"
