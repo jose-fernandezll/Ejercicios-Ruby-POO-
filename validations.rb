bf108e2a340e2efe9741a3af16d74e15fc240126
jose-fernandezll <jose.ismael.fernandez001@gmail.com>
2024-03-15 12:08:25 -0500

    module validation optimized

--- validations.rb
+++ validations.rb
@@ -1,16 +1,12 @@
 module Validations
   def must_be_positive(number, cadena)
-    begin
-      raise " #{cadena} must be positive" if number.to_f.negative?
+    number = number.to_f
 
-      number
-    rescue Exception => error
-      while number.negative?
-        puts error.message
-        number = gets.chomp.to_f
-        number.is_a?(Float)
-      end
-      number
+    until number.positive?
+      puts " #{cadena} must be positive "
+      number = gets.chomp.to_f
     end
+
+    number
   end
 end
