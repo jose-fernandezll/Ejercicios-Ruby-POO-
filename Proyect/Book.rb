fcb79dfb3bce0a5ea7a89faf4f207ff48164fb60
jose-fernandezll <jose.ismael.fernandez001@gmail.com>
2024-03-16 22:34:53 -0500

    defining structure of user and book classes

---
+++ Proyect/Book.rb
@@ -0,0 +1,8 @@
+class Book
+  def initialize(title, autor, year, state)
+    @title = title
+    @autor = autor
+    @publication_year = year
+    @state = state
+  end
+end
---
+++ Proyect/User.rb
@@ -0,0 +1,7 @@
+class User
+  def initialize(username, password, rol)
+    @username = username
+    @password = password
+    @rol = rol
+  end
+end
